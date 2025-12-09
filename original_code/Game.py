f1a46e75c4b7729b97291f8cccc9de99c2e5c1f3
nahom70 <terrefen@berea.edu>
2025-12-09 00:59:31 -0500

    Worked on Game testsuit, added enemeies and interaction with the enemies. Updates score and lives. Game over screen working

--- original_code/Game.py
+++ original_code/Game.py
@@ -3,6 +3,7 @@ import pygame
 import random
 from Player import Player
 from Blocks import Blocks
+from enemy import Enemy
 
 class Game:
 
@@ -22,10 +23,12 @@ class Game:
         self.mario = Player()
         self.blocks = Blocks()
 
-
         self.block_positions = []
         self.generate_blocks()
 
+        # Enemies list
+        self.enemies = []
+        self.spawn_enemies()
 
 
     def generate_blocks(self):
@@ -38,14 +41,13 @@ class Game:
         for _ in range(num_groups):
 
 
-            group = self.blocks.create_blocks(3, 1)
-
+            rand_normal = random.randint(2, 4)
+            rand_coins = random.randint(0, 1)
+            group = self.blocks.create_blocks(rand_normal, rand_coins)
 
             random.shuffle(group)
 
-
             group_start_x = start_x + random.randint(200, 300)
-
 
             for i, block in enumerate(group):
                 x = group_start_x + i * block_width
@@ -53,33 +55,61 @@ class Game:
                 rect = block.get_rect(topleft=(x, y))
                 self.block_positions.append((block, rect))
 
-
             start_x = group_start_x + 600
 
 
+    def spawn_enemies(self):
+        self.enemies = []
+
+        for i in range(10):  # number of enemies
+            x = random.randint(400, 5000)
+            y = 480
+            enemy = Enemy(x, y)
+            self.enemies.append(enemy)
+
+    def mario_enemy_interaction(self):
+        for enemy in self.enemies[:]:
+            if self.mario.rect.colliderect(enemy.rect):
+
+                # --- 1. MARIO STOMPS ENEMY (TOP HIT) ---
+                if (
+                        self.mario.y_velocity > 0 and
+                        self.mario.rect.bottom <= enemy.rect.top + 15
+                ):
+                    self.score += 1*100 # add score
+                    self.mario.y_velocity = -15  # bounce
+                    self.enemies.remove(enemy)  # kill enemy
+
+                # --- 2. MARIO HIT FROM SIDE OR BOTTOM ---
+                else:
+                    self.life -= 1
+                    # Knock Mario back a bit
+                    self.mario.x -= 25
+                    self.mario.rect.x = self.mario.x
 
     def load_sprite(self):
         self.camera_x = self.mario.x - 100
 
-
+        # Ground tiles
         ground = pygame.image.load("../image/floor2.jpg")
         for i in range(30):
             ground_x = (452 * i - self.camera_x) - 100
             self.screen.blit(ground, (ground_x, 542))
 
-
+        # Clouds
         cloud = pygame.image.load("../image/cloud_2.png")
         for i in range(1, 30):
             cloud_x = (400 * i * math.sqrt(i) - self.camera_x)
             self.screen.blit(cloud, (cloud_x, math.sin(1 / i) * 30 + 50))
 
-
+        # Flowers
         flowers = [
             pygame.image.load("../image/flower0.png"),
             pygame.image.load("../image/flower1.png"),
             pygame.image.load("../image/flower2.png"),
             pygame.image.load("../image/flower3.png")
         ]
+
         for i in range(50):
             flowers_x = (150 * i - self.camera_x)
             self.screen.blit(flowers[0], (flowers_x, 509))
@@ -98,12 +128,11 @@ class Game:
         self.screen.blit(font_primary.render('LIVES', True, "white"), (700, 20))
         self.screen.blit(font_secondary.render(str(self.life), True, "white"), (730, 50))
 
-        # Logo
+
         super_mario_logo = pygame.image.load("../image/SUPERMARIO_LOGO_BIG.png")
         SML_size = super_mario_logo.get_size()
         pygame.draw.rect(self.screen,"#FF746C",(30-self.camera_x, 90 ,SML_size[0]+20,SML_size[1]+20),0,20)
         self.screen.blit(super_mario_logo, (40 - self.camera_x, 100))
-
 
 
         for block, rect in self.block_positions:
@@ -111,13 +140,49 @@ class Game:
             self.screen.blit(block, (draw_x, rect.y))
 
 
+        for enemy in self.enemies:
+            draw_x = enemy.rect.x - self.camera_x
+            self.screen.blit(enemy.image, (draw_x, enemy.rect.y))
+
 
         self.screen.blit(self.mario.character, (100, self.mario.y))
 
+        # Debug Mario hitbox
+        pygame.draw.rect(
+            self.screen,
+            (0, 255, 0),
+            (self.mario.rect.x - self.camera_x, self.mario.rect.y, self.mario.rect.width, self.mario.rect.height),
+            2
+        )
 
+    def game_over_screen(self):
+        font = pygame.font.SysFont('../fonts/SuperMario256.ttf', 70)
+        font_score = pygame.font.SysFont('../fonts/SuperMario256.ttf', 35)
+
+        text = font.render("GAME OVER", True, (255, 0, 0))
+        text_rect = text.get_rect(center=(self.size[0] // 2, self.size[1] // 2))
+
+        text_score = font_score.render(f"SCORE {self.score}", True, (255, 255, 0))
+
+        text_score_rect = text_score.get_rect()
+        text_score_rect.centerx = text_rect.centerx
+        text_score_rect.top = text_rect.bottom + 20
+
+
+        waiting = True
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    waiting = False
+                    self.is_running = False
+
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(text, text_rect)
+            self.screen.blit(text_score, text_score_rect)
+            pygame.display.update()
+            self.clock.tick(10)
 
     def run(self):
-
         while self.is_running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -130,16 +195,26 @@ class Game:
             key = pygame.key.get_pressed()
             self.camera_x = self.mario.x - 100
 
+
             self.mario.movement(key, self.camera_x)
             self.mario.collide_with_blocks(self.block_positions)
 
+            # Enemy updates
+            for enemy in self.enemies:
+                enemy.update(self.block_positions)
+            self.mario_enemy_interaction()
+
+            if self.life <= 0:
+                self.game_over_screen()
+                return
+
             pygame.display.update()
             self.clock.tick(24)
-
 
 
 def main():
     super_mario = Game()
     super_mario.run()
 
-main()
+if __name__ == "__main__":
+    main()
---
+++ original_code/enemy.py
@@ -0,0 +1,57 @@
+import pygame
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        self.image = pygame.image.load("../image/goombas.png")
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+        self.speed = 2
+        self.direction = -1
+
+        self.y_velocity = 0
+        self.gravity = 1
+        self.on_ground = False
+
+
+        self.world_left = 0
+        self.world_right = 5000
+
+    def apply_gravity(self):
+        self.y_velocity += self.gravity
+        self.rect.y += self.y_velocity
+
+    def move(self):
+        self.rect.x += self.direction * self.speed
+
+    def update(self, blocks):
+
+        self.move()
+        self.apply_gravity()
+        if self.rect.x <= self.world_left:
+            self.rect.x = self.world_left
+            self.direction = 1
+
+        if self.rect.x >= self.world_right:
+            self.rect.x = self.world_right
+            self.direction = -1
+
+
+        for block_img, rect in blocks:
+            if self.rect.colliderect(rect):
+
+
+                if self.direction > 0 and self.rect.right > rect.left:
+                    self.rect.right = rect.left
+                    self.direction = -1
+
+                elif self.direction < 0 and self.rect.left < rect.right:
+                    self.rect.left = rect.right
+                    self.direction = 1
+
+        #
+        if self.rect.bottom >= 480:
+            self.rect.bottom = 540
+            self.y_velocity = 0
+            self.on_ground = True
