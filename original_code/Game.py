54115c986a6b08ac914b3c5c21863b13c243d8e8
Ksmun <Ksmun@berea.edu>
2025-12-09 02:24:28 -0500

    updated Game.py to make flowers random in a straight line

--- original_code/Game.py
+++ original_code/Game.py
@@ -23,14 +23,43 @@ class Game:
         self.mario = Player()
         self.blocks = Blocks()
 
+        # Blocks
         self.block_positions = []
         self.generate_blocks()
 
-        # Enemies list
+        # Enemies
         self.enemies = []
         self.spawn_enemies()
 
+        # Flowers
+        self.flower_positions = []
+        self.generate_flowers()
 
+
+    # -------------------------------
+    #  RANDOM FLOWER GENERATION
+    # -------------------------------
+    def generate_flowers(self):
+        self.flowers_images = [
+            pygame.image.load("../image/flower0.png"),
+            pygame.image.load("../image/flower1.png"),
+            pygame.image.load("../image/flower2.png"),
+            pygame.image.load("../image/flower3.png")
+        ]
+
+        self.flower_positions = []
+        x = 0
+        y = 510  # straight line height
+
+        for _ in range(50):  # number of flowers
+            img = random.choice(self.flowers_images)
+            x += random.randint(80, 200)  # random horizontal spacing
+            self.flower_positions.append((img, x, y))
+
+
+    # -------------------------------
+    #  RANDOM BLOCK GENERATION
+    # -------------------------------
     def generate_blocks(self):
         self.block_positions = []
 
@@ -39,14 +68,11 @@ class Game:
         start_x = 300
 
         for _ in range(num_groups):
-
-
             rand_normal = random.randint(2, 4)
             rand_coins = random.randint(0, 1)
             group = self.blocks.create_blocks(rand_normal, rand_coins)
 
             random.shuffle(group)
-
             group_start_x = start_x + random.randint(200, 300)
 
             for i, block in enumerate(group):
@@ -58,35 +84,41 @@ class Game:
             start_x = group_start_x + 600
 
 
+    # -------------------------------
+    #  SPAWN ENEMIES
+    # -------------------------------
     def spawn_enemies(self):
         self.enemies = []
-
         for i in range(10):  # number of enemies
             x = random.randint(400, 5000)
             y = 480
             enemy = Enemy(x, y)
             self.enemies.append(enemy)
 
+
+    # -------------------------------
+    #  MARIO & ENEMY INTERACTION
+    # -------------------------------
     def mario_enemy_interaction(self):
         for enemy in self.enemies[:]:
             if self.mario.rect.colliderect(enemy.rect):
 
                 # --- 1. MARIO STOMPS ENEMY (TOP HIT) ---
-                if (
-                        self.mario.y_velocity > 0 and
-                        self.mario.rect.bottom <= enemy.rect.top + 15
-                ):
-                    self.score += 1*100 # add score
-                    self.mario.y_velocity = -15  # bounce
-                    self.enemies.remove(enemy)  # kill enemy
+                if self.mario.y_velocity > 0 and self.mario.rect.bottom <= enemy.rect.top + 15:
+                    self.score += 100
+                    self.mario.y_velocity = -15
+                    self.enemies.remove(enemy)
 
                 # --- 2. MARIO HIT FROM SIDE OR BOTTOM ---
                 else:
                     self.life -= 1
-                    # Knock Mario back a bit
                     self.mario.x -= 25
                     self.mario.rect.x = self.mario.x
 
+
+    # -------------------------------
+    #  DRAW SPRITES
+    # -------------------------------
     def load_sprite(self):
         self.camera_x = self.mario.x - 100
 
@@ -103,46 +135,37 @@ class Game:
             self.screen.blit(cloud, (cloud_x, math.sin(1 / i) * 30 + 50))
 
         # Flowers
-        flowers = [
-            pygame.image.load("../image/flower0.png"),
-            pygame.image.load("../image/flower1.png"),
-            pygame.image.load("../image/flower2.png"),
-            pygame.image.load("../image/flower3.png")
-        ]
+        for img, x, y in self.flower_positions:
+            draw_x = x - self.camera_x
+            self.screen.blit(img, (draw_x, y))
 
-        for i in range(50):
-            flowers_x = (150 * i - self.camera_x)
-            self.screen.blit(flowers[0], (flowers_x, 509))
-            self.screen.blit(flowers[1], (flowers_x + 40, 509))
-
-
+        # HUD
         font_primary = pygame.font.SysFont('../fonts/SuperMario256.ttf', 40)
         font_secondary = pygame.font.SysFont("../fonts/SuperMario256.ttf", 35)
 
         self.screen.blit(font_primary.render('SCORE', True, "white"), (20, 20))
         self.screen.blit(font_secondary.render(str(self.score), True, "white"), (50, 50))
 
-
         self.screen.blit(font_primary.render('LIVES', True, "white"), (700, 20))
         self.screen.blit(font_secondary.render(str(self.life), True, "white"), (730, 50))
 
-
+        # Logo
         super_mario_logo = pygame.image.load("../image/SUPERMARIO_LOGO_BIG.png")
         SML_size = super_mario_logo.get_size()
         pygame.draw.rect(self.screen,"#FF746C",(30-self.camera_x, 90 ,SML_size[0]+20,SML_size[1]+20),0,20)
         self.screen.blit(super_mario_logo, (40 - self.camera_x, 100))
 
-
+        # Blocks
         for block, rect in self.block_positions:
             draw_x = rect.x - self.camera_x
             self.screen.blit(block, (draw_x, rect.y))
 
-
+        # Enemies
         for enemy in self.enemies:
             draw_x = enemy.rect.x - self.camera_x
             self.screen.blit(enemy.image, (draw_x, enemy.rect.y))
 
-
+        # Mario
         self.screen.blit(self.mario.character, (100, self.mario.y))
 
         # Debug Mario hitbox
@@ -153,6 +176,10 @@ class Game:
             2
         )
 
+
+    # -------------------------------
+    #  GAME OVER SCREEN
+    # -------------------------------
     def game_over_screen(self):
         font = pygame.font.SysFont('../fonts/SuperMario256.ttf', 70)
         font_score = pygame.font.SysFont('../fonts/SuperMario256.ttf', 35)
@@ -161,11 +188,9 @@ class Game:
         text_rect = text.get_rect(center=(self.size[0] // 2, self.size[1] // 2))
 
         text_score = font_score.render(f"SCORE {self.score}", True, (255, 255, 0))
-
         text_score_rect = text_score.get_rect()
         text_score_rect.centerx = text_rect.centerx
         text_score_rect.top = text_rect.bottom + 20
-
 
         waiting = True
         while waiting:
@@ -180,6 +205,10 @@ class Game:
             pygame.display.update()
             self.clock.tick(10)
 
+
+    # -------------------------------
+    #  GAME LOOP
+    # -------------------------------
     def run(self):
         while self.is_running:
             for event in pygame.event.get():
@@ -192,7 +221,6 @@ class Game:
 
             key = pygame.key.get_pressed()
             self.camera_x = self.mario.x - 100
-
 
             self.mario.movement(key, self.camera_x)
             self.mario.collide_with_blocks(self.block_positions)
@@ -213,6 +241,7 @@ class Game:
 def main():
     super_mario = Game()
     super_mario.run()
+
 
 if __name__ == "__main__":
     main()
