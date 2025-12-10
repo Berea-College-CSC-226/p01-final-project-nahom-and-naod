37aeeb146d3179f15d4ab9b52338e9ec6555019d
nahom70 <terrefen@berea.edu>
2025-12-10 03:35:24 -0500

    Added doc strings

--- original_code/Blocks.py
+++ original_code/Blocks.py
@@ -1,15 +1,19 @@
 import pygame
 
 class Blocks(pygame.sprite.Sprite):
+    """Loads block images and provides tools for creating block lists."""
+
     def __init__(self):
+        """Load normal and coin block images into memory."""
         super().__init__()
         self.normal_block = pygame.image.load("../image/Block_3.png")
         self.coin_block = pygame.image.load("../image/Prize_block_3.png")
 
     def create_blocks(self, num_blocks, num_coins):
+        """Return a list of normal and coin blocks based on given counts."""
         lst = []
-        for i in range(num_blocks):
+        for _ in range(num_blocks):
             lst.append(self.normal_block)
-        for i in range(num_coins):
+        for _ in range(num_coins):
             lst.append(self.coin_block)
         return lst
